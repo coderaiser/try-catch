08928e53f20a27daa73cf3c00d8af6d30e9777f4
coderaiser <coderaiser@cloudcmd.io>
2019-09-12 16:51:42 +0300

    feature(try-catch) add madrun

---
+++ madrun.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const {run} = require('madrun');
+
+module.exports = {
+    'test': () => 'tape \'test/*.js\'',
+    'lint': () => 'putout lib test madrun.js',
+    'fix:lint': () => run('lint', '--fix'),
+    'coverage': () => 'nyc npm test',
+    'report': () => 'nyc report --reporter=text-lcov | coveralls',
+};
+
--- test/try-catch.js
+++ test/try-catch.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const test = require('tape');
+const test = require('supertape');
 const tryCatch = require('..');
 
 test('try-catch: error', (t) => {
