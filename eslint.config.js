a9c01b98188b78070fff1a428b712447e6bf9d36
coderiaser <coderaiser@cloudcmd.io>
2026-02-13 00:50:14 +0200

    chore: lint

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,11 @@
 import {safeAlign} from 'eslint-plugin-putout';
+import {defineConfig} from 'eslint/config';
+import {matchToFlat} from '@putout/eslint-flat';
 
-export default safeAlign;
+export const match = {
+    '**/test.js': {
+        'no-undef': 'off',
+    },
+};
+
+export default defineConfig([safeAlign, matchToFlat(match)]);
